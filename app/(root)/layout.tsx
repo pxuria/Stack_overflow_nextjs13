06daa1624ff082153f4e483bf99a6757989586e3
pxuria <pouriamesram@gmail.com>
2024-09-06 02:52:18 -0700

    create theme logic

---
+++ app/(root)/layout.tsx
@@ -0,0 +1,17 @@
+const Layout = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <main className="background-light850_dark100">
+      Navbar
+      <div className="flex">
+        left sidebar
+        <section className="flex min-h-screen flex-1 flex-col px-6 pb-6 pt-36 max-md:pb-14 sm:px-14">
+          <div className="mx-auto w-full max-w-5xl">{children}</div>
+        </section>
+        right sidebar
+      </div>
+      Toasater
+    </main>
+  );
+};
+
+export default Layout;
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,15 +1,11 @@
-import { ClerkProvider, SignInButton, SignedIn, SignedOut, UserButton } from "@clerk/nextjs";
+import { ClerkProvider } from "@clerk/nextjs";
 import type { Metadata } from "next";
 import { Inter, Space_Grotesk } from "next/font/google";
-import "./globals.css";
+import React from "react";
 
-export const metdata: Metadata = {
-  title: "dev_overflow",
-  description: "A community-driven platformfor aasking and answering questions.",
-  icons: {
-    icon: "/assets/images/site-logo.svg",
-  },
-};
+import { ThemeProvider } from "@/context/ThemeProvider";
+import "../styles/prism.css";
+import "./globals.css";
 
 const inter = Inter({
   subsets: ["latin"],
@@ -23,27 +19,30 @@ const spaceGrotesk = Space_Grotesk({
   variable: "--font-spaceGrotesk",
 });
 
+export const metadata: Metadata = {
+  title: "DevFlow",
+  description:
+    "A community-driven platform for asking and answering programming questions. Get help, share knowledge, and collaborate with developers from around the world. Explore topics in web development, mobile app development, algorithms, data structures, and more.",
+  icons: {
+    icon: "/assets/images/site-logo.svg",
+  },
+};
+
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
-    <ClerkProvider
-      appearance={{
-        elements: {
-          formButtonPrimary: "primary-gradient",
-          footerActionLink: "primary-text-gradient hover:text-primary-500",
-        },
-      }}
-    >
-      <html lang="en">
-        <body className={`${inter.variable} ${spaceGrotesk.variable}`}>
-          <SignedOut>
-            <SignInButton />
-          </SignedOut>
-          <SignedIn>
-            <UserButton />
-          </SignedIn>
-          {children}
-        </body>
-      </html>
-    </ClerkProvider>
+    <html lang="en">
+      <body className={`${inter.variable} ${spaceGrotesk.variable}`}>
+        <ClerkProvider
+          appearance={{
+            elements: {
+              formButtonPrimary: "primary-gradient",
+              footerActionLink: "primary-text-gradient hover:text-primary-500",
+            },
+          }}
+        >
+          <ThemeProvider>{children}</ThemeProvider>
+        </ClerkProvider>
+      </body>
+    </html>
   );
 }
