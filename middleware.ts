5abd38ab4a3ab557f0170e180de52fd816551424
pxuria <pouriamesram@gmail.com>
2024-09-23 09:04:13 -0700

    update middleware

--- middleware.ts
+++ middleware.ts
@@ -1,6 +1,7 @@
 import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 
-const isProtectedRoute = createRouteMatcher(["/sign-in(.*)", "/sign-up(.*)"]);
+// const isProtectedRoute = createRouteMatcher(["/sign-in(.*)", "/sign-up(.*)"]);
+const isProtectedRoute = createRouteMatcher([]);
 
 export default clerkMiddleware((auth, request) => {
   if (isProtectedRoute(request)) {
