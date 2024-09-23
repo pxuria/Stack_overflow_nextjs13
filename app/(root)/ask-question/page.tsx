3ebeffb51793a7c56e65e23c437c142802d9d80b
pxuria <pouriamesram@gmail.com>
2024-09-23 14:35:08 -0700

    fix ask question issue

--- app/(root)/ask-question/page.tsx
+++ app/(root)/ask-question/page.tsx
@@ -8,13 +8,12 @@ const Page = async () => {
   if (!userId) redirect("/sign-in");
 
   const mongoUser = await getuserById({ userId });
-  console.log(mongoUser);
 
   return (
     <div>
       <h1 className="h1-bold text-dark100_light900">Ask a question</h1>
       <div className="mt-9">
-        <Question mongoUserId={JSON.stringify(mongoUser._id)} />
+        <Question mongoUserId={JSON.stringify(mongoUser?._id)} />
       </div>
     </div>
   );
